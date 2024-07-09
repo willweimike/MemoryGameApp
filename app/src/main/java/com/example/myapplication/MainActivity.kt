f119784c947848628afd855153dbd42f4f61a71b
Mike <mike20220101@gmail.com>
2024-07-09 19:57:57 +0800

    Add create activity usage

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -49,6 +49,10 @@ class MainActivity : AppCompatActivity() {
         tvNumMoves = findViewById(R.id.tvNumMoves)
         tvNumPairs = findViewById(R.id.tvNumPairs)
 
+        val intent = Intent(this, CreateActivity::class.java)
+        intent.putExtra(EXTRA_BOARD_SIZE, BoardSize.MEDIUM)
+        startActivity(intent)
+
         setupBoard()
     }
 
